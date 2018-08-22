6069669275216c1c9abdd7e2f346720026d7f806
Kunal Mehta <kunalm@fb.com>
2018-08-22 10:40:43 -0700

    Don't use is-expressions yet
    Accidentally synced via codemod.

--- src/Framework/HackTestCase.php
+++ src/Framework/HackTestCase.php
@@ -327,10 +327,10 @@ class HackTestCase {
   }
 
   public static function computeExpectedExceptionCode(mixed $exception_code): ?int {
-    if ($exception_code is int) {
+    if (is_int($exception_code)) {
       return $exception_code;
     }
-    if (!($exception_code is string)) {
+    if (!is_string($exception_code)) {
       return null;
     }
     $int = Str\to_int($exception_code);
