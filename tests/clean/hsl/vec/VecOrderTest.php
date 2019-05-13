6dec0dbfbc83225ce4a0a4393ed6cd11636116d0
Fred Emmott <fe@fb.com>
2019-05-13 12:43:09 -0700

    another lint fix

--- tests/clean/hsl/vec/VecOrderTest.php
+++ tests/clean/hsl/vec/VecOrderTest.php
@@ -8,7 +8,7 @@
  *
  */
 
-use namespace HH\Lib\{C, Math, Str, Vec};
+use namespace HH\Lib\{C, Str, Vec};
 use function Facebook\FBExpect\expect; // @oss-enable
 use type Facebook\HackTest\{DataProvider, HackTest}; // @oss-enable
 // @oss-disable: use InvariantViolationException as InvariantException;
